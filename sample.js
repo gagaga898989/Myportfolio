8c0e51ccccd024a098e629f55e8c02f211fd28ba
KF <133193484+gagaga898989@users.noreply.github.com>
2024-04-15 20:51:41 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,3 +6,15 @@ $(function() {
 		$('.start').fadeOut(500);
 	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
+
+// ボタン要素を作成
+let btn = document.createElement("button");
+
+// ボタンのテキストを設定
+btn.innerHTML = "Click Me";
+
+// ボタンを追加する場所を選択
+let target = document.getElementById("target");
+
+// ボタンを追加
+target.appendChild(btn);
